742c47420a6e6451100531f090d3ee10f61354dc
Sbrunelli433 <brunes46@gmail.com>
2019-07-08 07:06:02 -0500

    put my bootstrap in the right place and now things are pretty! reformatted my 'Add Movie' modal for spacing purposes and now it also looks as handsome as my mom says I am.

--- WebAPITest/WebAPITest/App_Start/BundleConfig.cs
+++ WebAPITest/WebAPITest/App_Start/BundleConfig.cs
@@ -20,7 +20,7 @@ namespace WebAPITest
                       "~/Scripts/bootstrap.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
+                      "~/Content/Solar-bootstrap.css",
                       "~/Content/site.css"));
         }
     }
